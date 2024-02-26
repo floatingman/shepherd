e1dc843386bc0584eddc991912b7f49aa9cbbb0a
Daniel Newman <dan@danlovesprogramming.com>
2024-02-26 15:32:15 -0600

    Moved steveClient instantiation into the poll, to make sure the management.cattle.io.setting schema is properly found. This has been causing intermittent issues in recurring runs.
    WIP - Generated interfaces for management.cattle.io removing clientset
    
    WIP - Generated interfaces for management.cattle.io removing clientset
    
    WIP - Generated interfaces for management.cattle.io
    
    WIP - Generated interfaces for management.cattle.io

--- generator.go
+++ generator.go
@@ -1,4 +1,5 @@
-//go:generate go run pkg/codegen/generator/cleanup/main.go
-//go:generate go run pkg/codegen/main.go
-
+//go:generate go mod vendor
+//go:generate go run pkg/codegen/generator/cleanup/main.go -mod vendor
+//go:generate go run pkg/codegen/main.go -mod vendor
+//go:generate rm -rf vendor
 package main
